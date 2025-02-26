5ba55605e80d36bb0cf94ad8cde48ee8a2dc302e
Saket <saketbhatnagar2@gmail.com>
2025-02-27 00:50:13 +0530

    Fix mongo db uri to be passed through env

---
+++ backend/src/lib/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const MONGO_URI = "mongodb+srv://saketbhatnagar2:bmwm340i!@cluster0.bunwk.mongodb.net/chats_db?retryWrites=true&w=majority&appName=Cluster0"
+
+
+export const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(MONGO_URI)
+        console.log(`Mongo DB connected : ${conn.connection.host}`)
+
+    }catch(error){
+        console.log(`connectDB error: ${error.message}`)
+        
+    }
+};
