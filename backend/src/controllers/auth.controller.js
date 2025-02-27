47c5b1e7b3b73d09cdf6c40791e307e5571ec081
Saket <saketbhatnagar2@gmail.com>
2025-02-27 12:07:03 +0530

    authentication routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -88,6 +88,8 @@ export const login = async (req,res) =>{
 
 };
 
+
+
 export const logout = (req,res) =>{
     //clear cookies:
     try{
@@ -103,9 +105,37 @@ export const logout = (req,res) =>{
 };
 
 
-
-
-
+export const updateProfile = async (req, res) => {
+    try {
+      const { profilePic } = req.body;
+      const userId = req.user._id;
+  
+      if (!profilePic) {
+        return res.status(400).json({ message: "Profile pic is required" });
+      }
+  
+      const uploadResponse = await cloudinary.uploader.upload(profilePic);
+      const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { profilePic: uploadResponse.secure_url },
+        { new: true }
+      );
+  
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      console.log("error in update profile:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
+  
+  export const checkAuth = (req, res) => {
+    try {
+      res.status(200).json(req.user);
+    } catch (error) {
+      console.log("Error in checkAuth controller", error.message);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
 
 
 console.log("HELLO from auth controller")
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,38 @@
+//midleware for authentication
+
+import jwt from "jsonwebtoken"
+import User from "../models/user.model.js"
+
+export const protectRoute = async (req,res,next)=>{
+
+    try{
+        const token = req.cookies.jwt
+        if(!token){
+            return res.status.send(401).json({
+                message:"unauthorized - no token provided"
+
+            });        
+        
+        }
+
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+        if(!decoded){
+            return res.status(401).json({message:"Unauthorized-invalid token"})
+
+        }
+        const user = await User.findById(decoded.UserId).select("-password")
+        if (!user){
+            return res.status(404).json({message:"user not found"})
+
+        }
+
+        req.user = user
+
+        next()
+
+    }   catch(error){
+        res.status(500).json({message:"internal server error"})
+        console.log("Error",error)
+    }
+}
