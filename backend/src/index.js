406afcec9bcc1aa0335c192dca4ee9a28cf391a5
Saket <saketbhatnagar2@gmail.com>
2025-02-27 12:08:12 +0530

    message schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import {connectDB} from "./lib/db.js"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
 dotenv.config()
 const PORT = process.env.PORT;
 
@@ -12,7 +14,7 @@ const app = express();
 
 
 app.use(express.json());
-
+app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 
 
